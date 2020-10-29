2f08be8b399e7ec0d549e3529829d6fcfddbba9f
PIYUSH KUMAR GUPTA <piyush.gupta_cs19@gla.ac.in>
2020-10-29 23:17:27 +0530

    Project Completed .

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -1,4 +1,56 @@
 package thread.tortoise;
 
-public class Tortoise {
-}
+import javax.swing.*;
+
+public class Tortoise implements Runnable {
+
+    private static final int MILESTONES = 5;
+
+    private final Thread tortoise;
+
+    public Tortoise() {
+        this.tortoise = new Thread(this, "Tortoise");
+    }
+
+    public Thread gettortoise() {
+        return tortoise;
+    }
+
+    @Override
+    public void run() {
+        for (int index = 0; index < MILESTONES; index++) {
+            switch (index) {
+                case 0:
+                    System.out.println("The tortoise has started from the start line!");
+                    break;
+                case 1:
+                    System.out.println("The tortoise has reached the Cool River!");
+                    break;
+                case 2:
+                    System.out.println("The tortoise has reached the Mountain Hill!");
+                    break;
+                case 3:
+                    System.out.println("The tortoise has reached the Big Oak Tree!");
+                   break;
+
+
+                case 4:
+                    System.out.println("The tortoise has reached the finish line!");
+                    break;
+            }
+            try {
+                Thread.sleep(1500L);
+            } catch (InterruptedException e) {
+                System.err.println("The tortoise has lost the path!");
+            }
+        }
+        JOptionPane.showConfirmDialog(
+                null,
+                "The tortoise has completed the race!",
+                "Rabbit",
+                JOptionPane.OK_CANCEL_OPTION
+        );
+    }
+
+    }
+
